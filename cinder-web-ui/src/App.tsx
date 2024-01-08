b7a7c8f8ebe0e433a252b2c398f887ab66655c0c
Cam Stuart <cam.asoftware@gmail.com>
2024-01-08 12:13:14 +1100

    basic styles and sidebar

--- cinder-web-ui/src/App.tsx
+++ cinder-web-ui/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import {Button, Heading, Text} from "@radix-ui/themes";
+import Layout from "./layout.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        build the fucking app already!
-      </p>
-    </>
-  )
+    return (
+
+            <Layout>
+            <div>
+                <a href="/">
+                    <img src='/cinder-logo.svg' width={'200px'} alt="Cinder logo"/>
+                </a>
+            </div>
+            <Heading>Cinder - Fire Awareness</Heading>
+            <div>
+                <Button variant="solid" onClick={() => setCount((count) => count + 1)}>
+                    Count is {count}
+                </Button>
+            </div>
+            <Text>build the fucking app already!</Text>
+            </Layout>
+    )
 }
 
 export default App
